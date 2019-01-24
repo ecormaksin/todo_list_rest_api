0ff9d3f1ca2f9e938117c27dfdcb8ee14fc05022
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2019-01-24 22:08:14 +0900

    first commit

---
+++ src/main/java/com/example/todo_list_rest_api/TodoListRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.todo_list_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListRestApiApplication.class, args);
+	}
+
+}
+
